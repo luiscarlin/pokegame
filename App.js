9be45537e06d16f36b5c55991a64132f797dcb94
Luis Carlin <luiscarlin@gmail.com>
2020-02-28 11:58:48 -0500

    get a pokemon

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Pokedex from 'pokedex-promise-v2';
+// import Pokedex from 'pokeapi-js-wrapper';
+// const P = new Pokedex.Pokedex();
 
 function App() {
+  useEffect(() => {
+    const P = new Pokedex();
+
+    console.log('hello');
+
+    P.getPokemonByName('eevee')
+      .then(response => console.log(response))
+      .catch(console.log);
+  }, []);
   return (
     <SafeAreaView>
       <Text>Welcome to Pokemon Game!</Text>
